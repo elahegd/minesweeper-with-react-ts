c0b91f231f1c1451560cd0e0b2c55f5e20724bbc
Elahe_gd <el.goodarzi2010@gmail.com>
2023-01-17 09:02:26 +0100

    GameOver component created by react-redux and using state, actions

---
+++ src/module/GameWithRedux/GameWithReactRedux/GameOver.tsx
@@ -0,0 +1,26 @@
+import React, { useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { RootState } from "../../../pages/store";
+import { actions } from "../index";
+
+import { GameOver as GameOverComponent } from "../../../components/Game/GameOver";
+
+export const GameOver = () => {
+    const dispatch = useDispatch();
+
+    const { isWin, isGameOver } = useSelector(({ game: { isWin, isGameOver } }: RootState) => ({
+        isWin, isGameOver
+    }));
+
+    const onReset = useCallback(
+        () => dispatch(actions.reset()), 
+        []
+    )
+
+    return (
+        <>
+            {isGameOver && <GameOverComponent onClick={onReset} isWin={isWin} />}
+        </>
+    )
+}
