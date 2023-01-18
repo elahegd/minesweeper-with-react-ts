21c4db98234c080b50abc5d74c4f411579e4b1de
Elahe_gd <el.goodarzi2010@gmail.com>
2023-01-18 11:36:11 +0100

    More test for Field file

--- src/helpers/Field.test.ts
+++ src/helpers/Field.test.ts
@@ -1,5 +1,5 @@
 import { fieldGenerator, generateFieldWithDefaultState, CellState } from "./Field";
-const { empty, bomb } = CellState;
+const { empty, bomb, hidden } = CellState;
 
 describe("Field Generator behaviour", () => {
     describe("generateFieldWithDefaultState", () => {
@@ -7,6 +7,20 @@ describe("Field Generator behaviour", () => {
             expect(generateFieldWithDefaultState(2)).toStrictEqual([
                 [empty, empty],
                 [empty, empty]
+            ])
+        });
+        it("3x3 empty field generator with default state", () => {
+            expect(generateFieldWithDefaultState(3)).toStrictEqual([
+                [empty, empty, empty],
+                [empty, empty, empty],
+                [empty, empty, empty]
+            ])
+        });
+        it("3x3 hidden field generator with default state", () => {
+            expect(generateFieldWithDefaultState(3, hidden)).toStrictEqual([
+                [hidden, hidden, hidden],
+                [hidden, hidden, hidden],
+                [hidden, hidden, hidden]
             ])
         })
     });
@@ -18,8 +32,11 @@ describe("Field Generator behaviour", () => {
             expect(() => fieldGenerator(2, -1)).toThrow(errorText);
             expect(() => fieldGenerator(2, 2)).toThrow(errorText);
         });
-        it("Smallest possible field without mine", () => {
+        it("Smallest possible field without bomb", () => {
             expect(fieldGenerator(1, 0)).toStrictEqual([[empty]]);
+        });
+        it("Smallest possible field with bomb", () => {
+            expect(fieldGenerator(1, 1)).toStrictEqual([[bomb]]);
         });
         it("2x2 filed with probability bomb", () => {
             expect(fieldGenerator(2, 1)).toStrictEqual([
@@ -46,7 +63,12 @@ describe("Field Generator behaviour", () => {
         });
 
         it("10x10 field with 25% probability", () => {
-            const field = fieldGenerator(10, 0.25);
+            const size = 10;
+            const mines = 25;
+
+            const probability = mines / (size * size);
+            const field = fieldGenerator(size, probability);
+            // const field = fieldGenerator(10, 0.25);
             const flattedFields = field.flat(); 
 
             // if line 66 change / to * just in the head of field is 99999.. and the tail is 00000...
